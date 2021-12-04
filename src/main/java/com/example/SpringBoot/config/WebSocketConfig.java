41759da46dc9eb6e4e733bdf2340b6f24446b030
siddharth-111 <siddharth.kumar111@gmail.com>
2021-12-04 17:50:19 +0530

    Configuration addition

--- src/main/java/com/example/SpringBoot/config/WebSocketConfig.java
+++ src/main/java/com/example/SpringBoot/config/WebSocketConfig.java
@@ -5,28 +5,30 @@ import com.example.SpringBoot.service.utils.ServiceHelper;
 import com.example.SpringBoot.websocket.handler.InstrumentsWsHandler;
 import com.example.SpringBoot.websocket.handler.QuotesWsHandler;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 import org.springframework.web.socket.client.WebSocketConnectionManager;
 import org.springframework.web.socket.client.standard.StandardWebSocketClient;
 
 @Configuration
 public class WebSocketConfig {
 
-    private final String instrumentsWsUri = "ws://localhost:8032/instruments";
-    private final String quotesWsUri = "ws://localhost:8032/quotes";
+    @Autowired
+    private Environment environment;
 
     @Autowired
     ConfigHelper configHelper;
 
     @Bean
     public WebSocketConnectionManager instrumentsWsConnectionManager() {
-        return configHelper.getWebSocketConnectionManager(this.instrumentsWsUri, new InstrumentsWsHandler());
+        return configHelper.getWebSocketConnectionManager(environment.getProperty("wsconfig.uri-prefix") + environment.getProperty("wsconfig.instruments-uri"), new InstrumentsWsHandler());
     }
 
     @Bean
     public WebSocketConnectionManager quotesWsConnectionManager() {
-        return configHelper.getWebSocketConnectionManager(this.quotesWsUri, new QuotesWsHandler());
+        return configHelper.getWebSocketConnectionManager(environment.getProperty("wsconfig.uri-prefix") + environment.getProperty("wsconfig.quotes-uri"), new QuotesWsHandler());
     }
 
 }
